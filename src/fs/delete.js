8d622533f15daadfb0ed59ad0d382f220304f5ae
ilya baranouski <iliuxa32@gmail.com>
2024-01-28 17:11:35 +0300

    feat:fs tasks done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
 const remove = async () => {
-    // Write your code here 
+    const fileName = fileURLToPath(import.meta.url);
+    const filePath = path.join(path.dirname(fileName), 'files', 'fileToRemove.txt');
+
+    try {
+        await fs.promises.rm(filePath)
+    } catch (error) {
+        throw new Error('FS operation failed')
+    }
 };
 
 await remove();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
 const rename = async () => {
-    // Write your code here 
+    const fileName = fileURLToPath(import.meta.url);
+    const oldName = path.join(
+        path.dirname(fileName), 'files', 'wrongFilename.txt');
+    const newName = path.join(
+        path.dirname(fileName), 'files', 'properFilename.md');
+
+    try {
+        await fs.promises.rename(oldName, newName)
+    } catch (error) {
+        throw new Error('FS operation failed')
+    }
 };
 
 await rename();

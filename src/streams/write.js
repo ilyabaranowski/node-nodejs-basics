1f11de2108839554f98f2e16bf013d7c08495f4b
ilya baranouski <iliuxa32@gmail.com>
2024-01-29 20:15:18 +0300

    feat:tasks done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-    // Write your code here 
+    const fileName = fileURLToPath(import.meta.url);
+    const parentPath = path.join(path.dirname(fileName), 'files', 'fileToWrite.txt');
+
+    const stream = fs.createWriteStream(parentPath);
+    process.stdin.on('data', (data) => stream.write(data));
+    process.on('SIGINT', () => process.exit());
+    process.on('exit', () => stream.end());
 };
 
 await write();
